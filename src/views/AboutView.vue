8b73bc0a632027d9650689bc20b54de18fb99a1f
chc952832 <littlekyuelf@gmail.com>
2022-07-24 17:12:25 +0800

    inital test

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+    <div class="input-group mb-3">
+      <div class="input-group-text">
+        <input
+          class="form-check-input mt-0"
+          type="checkbox"
+          value=""
+          aria-label="Checkbox for following text input"
+        />
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        aria-label="Text input with checkbox"
+      />
+    </div>
+    <input type="text">
+  </div>
+</template>
